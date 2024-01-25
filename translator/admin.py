79cbb3bcc8db72cf9399946f75fd3720ff7dbbad
Versun <me+github7604@versun.org>
2024-01-25 15:00:34 +0800

    add AzureAITranslator and optimize AITranslator

--- translator/admin.py
+++ translator/admin.py
@@ -4,13 +4,25 @@ from django.contrib import admin
 from django.conf import settings
 
 
-from .models import Translated_Content, OpenAITranslator, DeepLTranslator, MicrosoftTranslator
+from .models import Translated_Content, OpenAITranslator, DeepLTranslator, MicrosoftTranslator, AzureAITranslator
 from .tasks import translator_validate
 
 @admin.register(OpenAITranslator)
 class OpenAITranslatorAdmin(admin.ModelAdmin):
-    fields = ["name", "api_key", "base_url", "model", "prompt", "temperature", "max_tokens"]
-    list_display = ["name", "valid", "api_key", "model", "prompt", "temperature", "max_tokens","base_url"]
+    fields = ["name", "api_key", "base_url", "model", "prompt", "temperature", "top_p","frequency_penalty","presence_penalty","max_tokens"]
+    list_display = ["name", "valid", "api_key", "model", "prompt", "max_tokens","base_url"]
+
+    def save_model(self, request, obj, form, change):
+        logging.debug("Call save_model: %s", obj)
+        obj.valid = None
+        obj.save()
+        translator_validate(obj)  # 会执行一次obj.save()
+
+
+@admin.register(AzureAITranslator)
+class AzureAITranslatorAdmin(admin.ModelAdmin):
+    fields = ["name", "api_key", "endpoint", "version","deloyment_name", "prompt", "temperature", "top_p","frequency_penalty","presence_penalty","max_tokens"]
+    list_display = ["name", "valid", "api_key", "deloyment_name", "version", "prompt", "max_tokens","endpoint"]
 
     def save_model(self, request, obj, form, change):
         logging.debug("Call save_model: %s", obj)
