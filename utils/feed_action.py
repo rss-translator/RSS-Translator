0262f11d5e8f03767756065921512fac1fc111aa
Versun <me+github7604@versun.org>
2024-02-23 21:05:23 +0800

    add User-Agent

--- utils/feed_action.py
+++ utils/feed_action.py
@@ -18,7 +18,8 @@ def fetch_feed(url: str, modified: str = "", etag: str = "") -> Dict:
     response = None
     headers = {
         'If-None-Match': etag,
-        'If-Modified-Since': modified
+        'If-Modified-Since': modified,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
 
     client = httpx.Client()
