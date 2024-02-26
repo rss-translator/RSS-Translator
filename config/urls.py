83042214cd1e966d4c3a913679ed60087078e1d1
Versun <me+github7604@versun.org>
2024-02-26 03:44:47 +0000

    add charset=utf-8 to log

--- config/urls.py
+++ config/urls.py
@@ -29,7 +29,7 @@ def log(request):
     log_file = os.path.join(settings.DATA_FOLDER, 'app.log')
     with open(log_file, 'r') as file:
         log_content = file.read()
-    return HttpResponse(log_content, content_type='text/plain')
+    return HttpResponse(log_content, content_type='text/plain; charset=utf-8')
 
 
 if settings.DEMO:
