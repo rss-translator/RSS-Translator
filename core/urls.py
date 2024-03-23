7e3459060be7974db32bc2bdbfe8b5869d3c635f
Versun <me+github7604@versun.org>
2024-03-23 15:26:29 +0800

    fix slash issue

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from django.urls import re_path
 
 app_name = "core"
 urlpatterns = [
-    path("<str:feed_sid>/", views.rss, name="rss"),
+    #path("<str:feed_sid>/", views.rss, name="rss"),
+    re_path(r'(?P<feed_sid>[^/]+)/?$', views.rss, name='rss'),
 ]
