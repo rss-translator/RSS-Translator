e0b345333288c0ed59bc96214aaa6176db4f19bb
Versun <me+github7604@versun.org>
2024-05-13 19:28:30 +0800

    fix error:translate_feed: 'dict' object has no attribute 'value'

--- core/tasks.py
+++ core/tasks.py
@@ -281,7 +281,7 @@ def translate_feed(
                 #logging.info("Start Translate Content")
                 # original_description = entry.get('summary', None)  # summary, description
                 original_content = entry.get('content')
-                content = original_content[0].value if original_content else entry.get('summary')
+                content = original_content[0].get('value') if original_content else entry.get('summary')
 
                 if content:
                     translated_summary, tokens, characters, need_cache = content_translate(content,
