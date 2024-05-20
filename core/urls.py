1ae059c6ad3b6f6b1c4ca3fb7f433787adc34f56
Versun <me+github7604@versun.org>
2024-05-20 15:40:14 +0800

    add category url

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     #path("filter/<str:name>", views.filter, name="filter"),
     path("all/<str:name>", views.all, name="all"),
     path("all/<str:name>/", views.all, name="all"),
+    path("category/<str:category>", views.category, name="category"),
     path("category/<str:category>/", views.category, name="category"),
     path('json/<str:feed_sid>/', views.rss_json, name='json'),
     re_path(r'(?P<feed_sid>[^/]+)/?$', views.rss, name='rss'),
