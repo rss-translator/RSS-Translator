be43ae18f28a6752f926fa9612202e8eba19f3ec
Versun <me+github7604@versun.org>
2025-05-31 17:03:05 +0800

    更新删除函数

--- core/signals.py
+++ core/signals.py
@@ -11,10 +11,16 @@ from .models import Feed
 
 @receiver(post_delete, sender=Feed)
 def delete_feed_xml(sender, instance, **kwargs):
-    logging.info("Call delete_xml: %s", instance.sid)
-    feed_file_path = f"{settings.DATA_FOLDER}/feeds/{instance.sid}.xml"
-    if os.path.exists(feed_file_path):
-        os.remove(feed_file_path)
+    logging.info("Call delete_xml: %s", instance.slug)
+    original_feed_file_path = f"{settings.DATA_FOLDER}/feeds/o_{instance.slug}.xml"
+    if os.path.exists(original_feed_file_path):
+        os.remove(original_feed_file_path)
+    translated_feed_file_path = f"{settings.DATA_FOLDER}/feeds/t_{instance.slug}.xml"
+    if os.path.exists(translated_feed_file_path):
+        os.remove(translated_feed_file_path)
+    json_feed_file_path = f"{settings.DATA_FOLDER}/feeds/t_{instance.slug}.json"
+    if os.path.exists(json_feed_file_path):
+        os.remove(json_feed_file_path)
 
 
 # @receiver(post_delete, sender=T_Feed)
