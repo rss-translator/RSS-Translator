83e7faebab4691eb4008bfe479c3c3302f50cd5c
Versun <me+github7604@versun.org>
2025-06-10 11:09:39 +0800

    移除signals

--- core/apps.py
+++ core/apps.py
@@ -6,6 +6,3 @@ class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
     verbose_name = ""
-
-    def ready(self):
-        from . import signals
